ceb7526ee08e000fd0c9c7735e42f649e2a85054
Africa <ndouken@gmail.com>
2020-12-15 01:37:45 +0100

    Card design

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["responsive", "hover", "focus", "active"],
+      fontSize: ["responsive", "hover"],
+    },
+  },
+  plugins: [],
+};
